f4f7993f65a43cda76084fd4e024d9b62e88ebdb
NoMaidenz <deathblossom247@gmail.com>
2023-02-17 13:16:19 -0500

    2

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const DOMSelectors = {
   outputContainer: document.querySelector("#output-container"),
 };
 
+
+const alphabet =[
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+
 function createObject() {
   const encrypt = DOMSelectors.inputEncrypt.value;
   const encrypted = DOMSelectors.inputEncrypted.value;
