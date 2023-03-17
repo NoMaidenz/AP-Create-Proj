88073d7716571d9060ab66473b00dc7df76d032a
NoMaidenz <deathblossom247@gmail.com>
2023-03-17 00:00:05 -0400

    thing

--- Premade encrypter/caeser-main.js
+++ Premade encrypter/caeser-main.js
@@ -1,28 +1,59 @@
 const alphabet = [
-    'A','B','C','D','E','F',
-    'G','H','I','J','K','L',
-    'M','N','O','P','Q','R',
-    'S','T','U','V','W','X',
-    'Y','Z'
-  ];
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-  const form = document.forms[0];
-  const output = document.getElementById('output');
+const form = document.forms[0];
+const output = document.getElementById("output");
 
-  
-  form.addEventListener ('submit' ,event => {
-    event.preventDefault();
-    output.innerHTML = [... form.plaintext.value ].map(char => encrypt(char)).join('');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  output.innerHTML = [...form.input.value]
+    .map((char) => encrypt(char))
+    .join("");
+});
+function decrypt() {
+  const shift = -Number(form.shift.value); //
+  if (alphabet.includes(char.toLowerCase())) {
+    const position = alphabet.indexOf(char);
+    const OldPosition = (position + shift) % 26;
+    return alphabet[OldPosition];
+  } else {
+    return char;
   }
-  );
+}
 
-  function encrypt(char) {
-    const shift = Number(form.shift.value);
-    if (alphabet.includes(char.toUpperCase()))
-    {
-        const position = alphabet.indexOf(char.toUpperCase());
-        const newPosition = (position + shift)%26;
-        return alphabet[newPosition]
-    }
-    else { return char }
+function encrypt(char) {
+  const shift = Number(form.shift.value);
+  if (alphabet.includes(char.toLowerCase())) {
+    const position = alphabet.indexOf(char);
+    const newPosition = (position + shift) % 26;
+    return alphabet[newPosition];
+  } else {
+    return char;
   }
+}
